53e41e950658c6f19e639ddfaac3d11d548f2d17
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-09-25 17:11:23 +0900

    week05 v0.4

--- week05/main.go
+++ week05/main.go
@@ -9,6 +9,6 @@ import (
 func main() {
 	fmt.Print("Input score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore := reader.ReadString('\n') // 1 variable but reader.ReadString returns 2 values
+	inputScore, err := reader.ReadString('\n') // err declared and not used
 	fmt.Println(inputScore)
 }
