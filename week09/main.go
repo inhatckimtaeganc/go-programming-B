f7c7e214dcaa0b0b82e161a20f904ac1bbecccda
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-10-23 17:31:23 +0900

    week09 v1.7

--- week09/main.go
+++ week09/main.go
@@ -2,13 +2,11 @@ package main
 
 import "fmt"
 
+func double(n int) {
+	n = n * 2
+}
 func main() {
-	a := 10 // var a int = 10
-	var pa *int
-	pa = &a
-
-	fmt.Println(a, *pa)
-	fmt.Println(&a, pa)
-	fmt.Printf("%T %T %T %T\n", a, *pa, &a, pa)
-	fmt.Println(&pa)
+	var a int = 5
+	double(a)
+	fmt.Printf("%d\n", a)
 }
