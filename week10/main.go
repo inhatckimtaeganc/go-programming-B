4ccb2f352ad87bc9e7e4f38517c379262eeebf19
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-10-30 17:15:43 +0900

    week10 v2.2

--- week10/main.go
+++ week10/main.go
@@ -7,8 +7,10 @@ import (
 )
 
 func main() {
-	fmt.Println(mymath.MyAbs(-99))
-	fmt.Println(mymath.MyAbs(71))
+	fmt.Println(mymath.MyPower(2, 9))
+	fmt.Println(mymath.MyPower(4, 3))
 	greeting.Hi()
 	greeting.Hello()
+	fmt.Println(mymath.MyAbs(-99))
+	fmt.Println(mymath.MyAbs(71))
 }
--- week10/src/mymath/mymath.go
+++ week10/src/mymath/mymath.go
@@ -1,6 +1,7 @@
 package mymath
 
-func MyPower(base int, exponent int) int {
+//func myPower(base int, exponent int) int {
+func MyPower(base int, exponent int) int { // 외부에 함수를 공개하려면 반드시 함수 이름의 첫 글자를 대문자로 작성한다.
 	result := 1
 	for i := 1; i <= exponent; i++ {
 		result = result * base
