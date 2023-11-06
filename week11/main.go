4a5e865ad94c46ac367b4a72948bec77ae51a898
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-06 17:15:14 +0900

    week11 v2.4

--- week11/main.go
+++ week11/main.go
@@ -15,4 +15,18 @@ func main() {
 	primes := [3]int{2, 3, 5} // 배열 리터럴로 primes 배열 초기화
 	fmt.Println(primes, primes[1])
 
+	test := [5]bool{true, true, true} // 초기화 하지 않은 배열 원소의 제로 값은 해당 원소의 타입의 제로값으로 결정된다
+	fmt.Println(test[3])
+
+	// for i := 0; i < 3; i++ {
+	// 	fmt.Println(primes[i])
+	// }
+
+	//fmt.Println(primes[3]) // 컴파일 에러. invalid argument: index 3 out of bounds [0:3]
+
+	i := 0
+	for i < 4 { // 실행시간 에러. panic: runtime error: index out of range [3] with length 3
+		fmt.Println(primes[i])
+		i++
+	}
 }
