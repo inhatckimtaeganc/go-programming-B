1f2f2675dc92db933d1a7505d5063cc88b9b0f0c
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-13 17:08:48 +0900

    week12 v2.9

--- week12/main.go
+++ week12/main.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// var s []int
-	// s = make([]int, 5)
-
-	// s := make([]int, 5) // 단축연산자 사용, 변수 선언과 동시에 메모리 할당, 원소들은 제로 값으로 초기화
-
-	s := []int{0, 0, 0, 0, 0} // 단축연산자 사용, 변수 선언과 동시에 메모리 할당 그리고 원소 초기화
+	s := []int{0, 0, 0, 0, 0} // 단축 연산자 및 슬라이스 리터럴 사용, 변수 선언과 동시에 메모리 할당 그리고 원소 초기화
 
 	s[4] = 100
 	s[0] = 7
@@ -17,4 +12,14 @@ func main() {
 	for _, value := range s {
 		fmt.Println(value)
 	}
+
+	copyS := s[1:4]
+	for _, value := range copyS {
+		fmt.Println(value)
+	}
+
+	test := [3]string{"inha", "go", "student"}
+	testS := test[:2] // testS := test[0:2]
+	fmt.Println(test, len(test))
+	fmt.Println(testS, len(testS))
 }
