7e1416ce2015b62308c7ef5228fe7034839fc849
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-13 17:31:31 +0900

    week12 v3.1

--- week12/main.go
+++ week12/main.go
@@ -3,30 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	s := []int{0, 0, 0, 0, 0} // 단축 연산자 및 슬라이스 리터럴 사용, 변수 선언과 동시에 메모리 할당 그리고 원소 초기화
+	a := []string{"a", "b", "c", "d"}
+	as := a[:2]
+	as[1] = "Z"
+	fmt.Println(a, as)
 
-	s[4] = 100
-	s[0] = 7
-	s[1] = 91
-
-	for _, value := range s {
-		fmt.Println(value)
-	}
-
-	copyS := s[1:4]
-	for _, value := range copyS {
-		fmt.Println(value)
-	}
-
-	test := [3]string{"inha", "go", "student"}
-	testS := test[:2] // testS := test[0:2]
-	testS2 := test[1:]
-
-	testS2[0] = "python"
-	//test[1] = "python"
-	//testS[2] = "python"
-
-	fmt.Println(test, len(test))
-	fmt.Println(testS, len(testS))
-	fmt.Println(testS2, len(testS2))
+	b := [4]int{4, 3, 2, 1}
+	bs := b[1:3]
+	fmt.Println(b, bs)
 }
