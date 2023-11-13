4701dab1785d761cbd318a430bd7eb13e403fa5f
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-13 17:50:46 +0900

    week12 v3.2

--- week12/main.go
+++ week12/main.go
@@ -10,7 +10,10 @@ func main() {
 	as := a[0:2]
 	as[1] = "Z"
 	c := append(a, "y")
+	//c := append(a, "y", "x")
 
+	c[0] = "q"
 	fmt.Println(a, len(a), cap(a))
 	fmt.Println(c, len(c), cap(c))
+	fmt.Printf("%x %x %x\n", &a[0], &as[0], &c[0])
 }
