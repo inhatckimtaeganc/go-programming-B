c9fce2b813e812cd8750e5597cffba647314cc69
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-20 16:52:23 +0900

    week13 v3.4

--- week13/main.go
+++ week13/main.go
@@ -4,6 +4,11 @@ import "fmt"
 
 func main() {
 	var a []string
-	fmt.Printf("%#v\n", a)
+	var b []bool
+	//a = make([]string, 4, 5)
+
+	b = append(b, true)
+	fmt.Printf("%#v %#v\n", a, b)
 	fmt.Println(a, len(a), cap(a))
+	fmt.Println(b, len(b), cap(b))
 }
