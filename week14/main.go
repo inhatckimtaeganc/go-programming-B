b0b7d822fae0c4f71822afb94c0563796914a22e
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-27 16:36:58 +0900

    week14 v3.6

---
+++ week14/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var games map[int]string
+	games = make(map[int]string)
+	// append
+	games[456] = "성기훈"
+	games[218] = "박해수"
+	games[067] = "강새벽"
+	games[001] = "오일남"
+	games[199] = "알리"
+	games[101] = "아이오아이"
+
+	//fmt.Println(games[067])
+	for _, v := range games {
+		fmt.Println(v)
+	}
+}
