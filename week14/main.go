d3ca04c318514432f2f6526abf96f4635bdf5cce
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-27 16:45:39 +0900

    week14 v3.8

--- week14/main.go
+++ week14/main.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var games map[int]string
-	games = make(map[int]string)
+	// var games map[int]string
+	// games = make(map[int]string)
+	games := make(map[int]string)
 	// append
 	games[456] = "성기훈"
 	games[218] = "박해수"
